ef8288fbb0fef0b8a8d268307a66e6c2fc815391
ShahnazKhanum16 <85401790+ShahnazKhanum16@users.noreply.github.com>
2021-06-06 17:15:20 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  var CurrYear = new Date().getFullYear();
+  
+return (
+   <footer>
+     <p>
+       Copyright  @  2021
+       </p>
+   </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+return (
+ <div className="note">
+   <h1> Javascripts and react.js </h1>
+   <p> This was an amazing bootcamp taken up by shauriya sir 
+      I have covered everything from scratch including Javascripts ,react.js,HTML.
+      I have learnt many tags,and some coding stuff also.I thank shauriya sir, who taught is bootcamp and made me learn.
+  
+   </p>
+   </div>
+  );
+}
+
+export default Note;
